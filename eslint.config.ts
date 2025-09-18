b01d12f50f83941d1866ec206c42d7899ee2a93c
from-other-place <usetheemail@myothergit.account>
2025-09-18 19:35:29 +0530

    Convert everything to typescript
    Still need to fix the linting and build process for ts.

--- eslint.config.ts
+++ eslint.config.ts
@@ -16,6 +16,10 @@ export default defineConfig([
       },
     },
     rules: {
+      '@typescript-eslint/restrict-template-expressions': [
+        'error',
+        { allowNumber: true },
+      ],
       '@typescript-eslint/no-unused-vars': [
         'error',
         {
--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import { userRouter } from './routers/userRouter.ts';
 import { indexRouter } from './routers/indexRouter.ts';
 import { errorHandler } from './errors.ts';
 
-const PORT = process.env.PORT ?? '3000';
+const PORT = process.env.PORT ?? 3_000;
 const { dirname } = import.meta;
 const app = express();
 
@@ -18,6 +18,6 @@ app.use('/', indexRouter);
 
 app.use(errorHandler);
 
-app.listen(Number(PORT), () => {
+app.listen(PORT, () => {
   console.log(`Express app listening on port ${PORT}!`);
 });
