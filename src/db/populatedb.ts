464a08540dcd84e07dc34a689b589d7f4b2be9ac
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2026-01-03 00:00:07 +0530

    Do the setup (finally)

--- src/db/populatedb.ts
+++ src/db/populatedb.ts
@@ -3,7 +3,7 @@ import { configDotenv } from 'dotenv';
 import fs from 'node:fs';
 import { Client } from 'pg';
 import path from 'node:path';
-// import { tableSeeds } from './tableSeeds.js';
+import { tableSeeds } from './tableSeeds.js';
 
 const { dirname } = import.meta;
 const parameter = process.argv.at(2);
@@ -32,12 +32,27 @@ configDotenv({
 const client = new Client(getConfig());
 await client.connect();
 await client.query(schemaSQL);
+const insertValuesFormat = (listOfStrings: string[]) =>
+  listOfStrings
+    .map((theString, index) => {
+      if (index % 2 === 0) {
+        if (index === listOfStrings.length - 1) return `${theString})`;
+        return `${theString}), `;
+      }
+
+      return `(${theString}`;
+    })
+    .join('');
 
 console.log('seeding...');
-// await Promise.all(
-//   Object.values(tableSeeds).map((seedSql) =>
-//     seedSql ? client.query(seedSql) : Promise.resolve(),
-//   ),
-// );
+await Promise.all(
+  Object.entries(tableSeeds).map(([tableName, tableData]) => {
+    if (!tableData) return Promise.resolve();
+    return client.query(
+      `INSERT INTO ${tableName} (${tableData.columns.join(', ')})
+       VALUES ${insertValuesFormat(tableData.values.map((valueGroup) => valueGroup.join(', ')))};`,
+    );
+  }),
+);
 await client.end();
 console.log('done');
