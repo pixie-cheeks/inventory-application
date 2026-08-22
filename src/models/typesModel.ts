0156cea2b4f1690ae8a5b050bc59370d50820419
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2026-08-22 22:54:31 +0530

    Update typesModel

--- src/models/typesModel.ts
+++ src/models/typesModel.ts
@@ -8,61 +8,50 @@ interface PokemonType {
 
 class DatabaseSeeder {
   pool: Client;
-  bam = 5;
 
-  getAllTypesDB = async (): Promise<PokemonType[]> => {
+  constructor(pool: Client) {
+    this.pool = pool;
+  }
+
+  async getAllTypesDB(): Promise<PokemonType[]> {
     const { rows } = await this.pool.query<PokemonType>('SELECT * FROM types;');
     return rows;
-  };
+  }
 
-  getType = async (id: number): Promise<PokemonType | undefined> => {
+  async getType(id: number): Promise<PokemonType | undefined> {
     const { rows } = await this.pool.query<PokemonType>(
       'SELECT * FROM types WHERE id = $1;',
       [id],
     );
     return rows.at(0);
-  };
+  }
 
-  addType = async ({ type_name }: Omit<PokemonType, 'id'>): Promise<void> => {
+  async addType({ type_name }: Omit<PokemonType, 'id'>): Promise<void> {
     await this.pool.query('INSERT INTO types (type_name) VALUES ($1);', [
       type_name,
     ]);
-  };
+  }
 
-  editType = async ({
+  async editType({
     id,
     new_name,
   }: {
     id: number;
     new_name: string;
-  }): Promise<void> => {
+  }): Promise<void> {
     await this.pool.query('UPDATE types SET type_name = $1 WHERE id = $2;', [
       new_name,
       id,
     ]);
-  };
-
-  deleteType = async (id: string): Promise<void> => {
-    await this.pool.query('DELETE FROM types WHERE id = $1;', [id]);
-  };
-
-  deleteAllTypes = async (): Promise<void> => {
-    await this.pool.query('DELETE FROM types;');
-  };
-
-  constructor(pool: Client) {
-    this.pool = pool;
   }
-  // async createType(
-  //   typeData: Omit<PokemonType, 'id'>,
-  // ): Promise<PokemonType | undefined> {
-  //   const result = await this.client.query<PokemonType>(
-  //     'INSERT INTO types (type_name, image_src) VALUES ($1, $2);',
-  //     [typeData.type_name, typeData.image_src],
-  //   );
 
-  //   return result.rows.at(0);
-  // }
+  async deleteType(id: string): Promise<void> {
+    await this.pool.query('DELETE FROM types WHERE id = $1;', [id]);
+  }
+
+  async deleteAllTypes(): Promise<void> {
+    await this.pool.query('DELETE FROM types;');
+  }
 }
 
 export { DatabaseSeeder };
