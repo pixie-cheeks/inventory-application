5cbc020dd399d261ef0fa02bacc64d63297f46e8
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2026-08-23 19:24:02 +0530

    Improve the seeding script

--- src/db/populateDatabase.ts
+++ src/db/populateDatabase.ts
@@ -2,36 +2,75 @@
 import fs from 'node:fs/promises';
 import path from 'node:path';
 import { pool } from './pool.js';
-import { dropTables, resetTables, seedTables } from './tableSeeder.js';
+import * as tableSeeder from './tableSeeder.js';
 
 const { dirname } = import.meta;
-const parameter = process.argv.at(2);
-const shouldReset = parameter === '-r' || parameter === '--reset';
-const shouldDrop = parameter === '-d' || parameter === '--drop';
-
 const schemaSqlPath = path.resolve(dirname, './schema.sql');
 const schemaSQL = await fs.readFile(schemaSqlPath, { encoding: 'utf8' });
 
-if (shouldDrop) {
-  console.log('Dropping tables first...');
-  await dropTables();
-  console.log('Successfully dropped all tables!');
-}
+const dropTables = async (): Promise<void> => {
+  console.log('Dropping tables...');
+  await tableSeeder.dropTables();
+  console.log(
+    'Successfully dropped all tables! Skipped if there were no tables.',
+  );
+};
 
-console.log('Creating tables...');
-await pool.query(schemaSQL);
-console.log(
-  'Creating tables was successful! Skipped if the tables were already created.',
-);
+const createTables = async (): Promise<void> => {
+  console.log('Creating tables...');
+  await pool.query(schemaSQL);
+  console.log(
+    'Creating tables was successful! Skipped if the tables were already created.',
+  );
+};
 
-if (shouldReset) {
+const resetTables = async (): Promise<void> => {
   console.log('Resetting tables...');
-  await resetTables();
-  console.log('Resetting tables was successful!');
+  try {
+    await tableSeeder.resetTables();
+    console.log('Resetting tables was successful!');
+  } catch {
+    console.error(
+      "Failed to reset the tables. Maybe the given tables don't exist yet.",
+    );
+  }
+};
+
+const seedTables = async (): Promise<void> => {
+  console.log('Seeding those tables...');
+  await tableSeeder.seedTables();
+  console.log('Seeding tables was successful!');
+};
+
+const doSeedingProcedure = async (): Promise<void> => {
+  const parameter = process.argv.at(3);
+  const shouldReset = parameter === '-r' || parameter === '--reset';
+  const shouldDrop = parameter === '-d' || parameter === '--drop';
+
+  if (shouldDrop) await dropTables();
+  await createTables();
+  if (shouldReset) await resetTables();
+  await seedTables();
+};
+
+switch (process.argv.at(2)) {
+  case 'drop': {
+    await dropTables();
+    break;
+  }
+  case 'reset': {
+    await resetTables();
+    break;
+  }
+  case 'seed': {
+    await doSeedingProcedure();
+    break;
+  }
+  default: {
+    console.log('Use the following subcommands: seed, reset or drop');
+  }
 }
 
-console.log('Seeding those tables...');
-await seedTables();
-
+console.log('Ending the pg pool...');
 await pool.end();
-console.log('Done! Successfully populated the databse.');
+console.log('Done!');
