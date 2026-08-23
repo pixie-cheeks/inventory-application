cfcba1f8834fbad3c37fb5b2497f1fc69b0412e2
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2026-08-23 17:45:24 +0530

    Add ownedPokemonTabelModel

---
+++ src/models/ownedPokemonTabelModel.ts
@@ -0,0 +1,65 @@
+import { BaseTableModel } from './baseTableModel.js';
+import { pool } from '../db/pool.js';
+import type { Pokemon } from './pokemonsModel.js';
+import type { Trainer } from './trainersModel.js';
+
+interface OwnedPokemon {
+  id: number;
+  pokemon_id: number;
+  trainer_id: number;
+}
+
+class OwnedPokemonTableModel extends BaseTableModel<OwnedPokemon> {
+  constructor() {
+    super(pool, 'owned_pokemons');
+  }
+
+  async getPokemonsByTrainerId(trainerId: number): Promise<Pokemon[]> {
+    const { rows } = await this.pool.query<Pokemon>(
+      /* sql */ `
+        SELECT
+          *
+        FROM
+          pokemons
+        WHERE
+          id IN (
+            SELECT
+              pokemon_id
+            FROM
+              ${this.tableName}
+            WHERE
+              trainer_id = $1
+          );
+      `,
+      [trainerId],
+    );
+    return rows;
+  }
+
+  async getTrainersByPokemonId(pokemonId: number): Promise<Trainer[]> {
+    const { rows } = await this.pool.query<Trainer>(
+      /* sql */ `
+        SELECT
+          *
+        FROM
+          trainers
+        WHERE
+          id IN (
+            SELECT
+              trainer_id
+            FROM
+              ${this.tableName}
+            WHERE
+              pokemon_id = $1
+          );
+      `,
+      [pokemonId],
+    );
+    return rows;
+  }
+}
+
+const ownedPokemonTable = new OwnedPokemonTableModel();
+
+export { ownedPokemonTable };
+export type { OwnedPokemon };
