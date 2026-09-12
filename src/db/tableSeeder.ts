9d60fb53ca6e1f7510d6e77d1bfa96a1cd0f3bf6
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2026-09-13 00:30:09 +0530

    Add seeds for the app

--- src/db/tableSeeder.ts
+++ src/db/tableSeeder.ts
@@ -12,26 +12,64 @@ type InsertionTrainer = Omit<Trainer, 'id'>;
 type InsertionOwnedPokemon = Omit<OwnedPokemon, 'id'>;
 
 const types: InsertionPokemonType[] = [
-  { type_name: 'electric' },
-  { type_name: 'fire' },
-  { type_name: 'water' },
-  { type_name: 'grass' },
+  {
+    type_name: 'Electric',
+    image_src:
+      'https://wallpapers.com/images/hd/electricity-pictures-t3rd8krvpro7yjlu.jpg',
+  },
+  {
+    type_name: 'Fire',
+    image_src:
+      'https://img.magnific.com/premium-photo/fire-natural-phenomenon-hannover_1048944-10478964.jpg',
+  },
+  {
+    type_name: 'Water',
+    image_src:
+      'https://thumbs.dreamstime.com/b/clear-water-glass-cup-generative-ai-high-quality-illustration-273613563.jpg',
+  },
+  {
+    type_name: 'Grass',
+    image_src:
+      'https://thumbs.dreamstime.com/b/green-grass-cup-isolated-white-background-raw-food-concep-concept-40233681.jpg',
+  },
 ];
 
 const trainers: InsertionTrainer[] = [
-  { trainer_name: 'Ash', trainer_description: 'Ash has a nice cap.' },
+  {
+    trainer_name: 'Ash Ketchum',
+    trainer_description: 'Ash has a nice cap.',
+    image_src:
+      'https://wallpapers.com/images/hd/ash-ketchum-pokemon-trainer-phzqjgbsyn6ei9sx.jpg',
+  },
+  {
+    trainer_name: 'Misty',
+    trainer_description: 'Misty likes to swim a lot.',
+    image_src:
+      'https://i.pinimg.com/originals/a4/6a/5a/a46a5afdf9eacb9c8b3334a8b4c176d1.png',
+  },
 ];
 
 const pokemons: InsertionPokemon[] = [
   {
     pokemon_name: 'Pikachu',
-    type_one: 'electric',
+    type_one: 'Electric',
     pokemon_description: 'An electric mouse pokemon.',
+    image_src:
+      'https://vignette.wikia.nocookie.net/ssbb/images/b/b8/025Pikachu_LG.png/revision/latest?cb=20190520161120&path-prefix=es',
   },
   {
     pokemon_name: 'Bulbasaur',
-    type_one: 'grass',
-    pokemon_description: 'A grass frog pokemon',
+    type_one: 'Grass',
+    pokemon_description: 'A grass frog pokemon.',
+    image_src:
+      'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/001.png',
+  },
+  {
+    pokemon_name: 'Psyduck',
+    type_one: 'Water',
+    pokemon_description: 'A water duck pokemon.',
+    image_src:
+      'https://www.pngmart.com/files/22/Psyduck-Pokemon-PNG-Photos.png',
   },
 ];
 
@@ -41,6 +79,7 @@ const pokemons: InsertionPokemon[] = [
 const owned_pokemons: InsertionOwnedPokemon[] = [
   { pokemon_id: 0, trainer_id: 0 },
   { pokemon_id: 1, trainer_id: 0 },
+  { pokemon_id: 2, trainer_id: 1 },
 ];
 
 const tableOrderArray = [
